c5fd2bc767c77bb4bbab39c848616e61305ccfd0
Mark JD <47066420+jaskiewm@users.noreply.github.com>
2021-08-01 12:40:35 -0400

    Added Overheating
    - Added overheating feature where too many bullets shot in a short time cause a pause in shooting
    - Overlay shows the overheating occurring (Slider)

--- Space_Game/Assets/Scripts/MainMenu.cs
+++ Space_Game/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,6 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
     public void QuitGame()
